b316f22923458cfa3832045f9da508766698e51b
dilshad <hmydilshadkt@gmail.com>
2025-12-19 15:27:50 +0530

    feat:changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,11 @@ import Secondcompact from "./component/Secondcompact";
 
 
 const App = () => {
-  
+
+  // TODO : Use the same font style from the figma
+  // TODO : Make the SideBar sticky only the productlist will scroll not the whole page
+  // TODO : Use the same color theme like figma 
+  // TODO : Component named would be meaning full and descriptive
   
   return (
     <Router>
